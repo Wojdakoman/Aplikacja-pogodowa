797a9aeeefc75a080a9f794faba8bfa57c27f7a0
jknike <61454002+jknike@users.noreply.github.com>
2021-01-28 16:54:43 +0100

    Geolocation of user

--- js/script.js
+++ js/script.js
@@ -5,3 +5,38 @@ $(document).on('click','#navicon',function(event){
 });
 //nav
 $(".page").first().toggle();
+
+
+//Funcja do onClick potem
+function getCurrentLocation(){
+
+    const successfulLookup = position => {
+        const { latitude, longitude } = position.coords;
+        fetch(`https://api.opencagedata.com/geocode/v1/json?q=${latitude}+${longitude}&key=5af07f4f6a8b4918a010d981b6175f78`)
+          .then(response => response.json())
+          .then(console.log);
+        
+          CallByCordinates(latitude, longitude)
+    }
+          
+    if(window.navigator.geolocation){
+        window.navigator.geolocation
+      .getCurrentPosition(successfulLookup, console.log);
+    }
+
+}
+
+//Na teraz by działo po otwarciu pliku
+const successfulLookup = position => {
+    const { latitude, longitude } = position.coords;
+    fetch(`https://api.opencagedata.com/geocode/v1/json?q=${latitude}+${longitude}&key=5af07f4f6a8b4918a010d981b6175f78`)
+      .then(response => response.json())
+      .then(console.log);
+    
+      CallByCordinates(latitude, longitude)
+}
+
+if(window.navigator.geolocation){
+    window.navigator.geolocation
+  .getCurrentPosition(successfulLookup, console.log);
+}
